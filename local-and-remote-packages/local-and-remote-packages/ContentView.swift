a9d52a18190dd0fe1054c7b1a1bf52558629478f
Jacob Christie <19879272+jjatie@users.noreply.github.com>
2020-07-16 11:27:05 -0300

    initial commit

---
+++ local-and-remote-packages/local-and-remote-packages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  local-and-remote-packages
+//
+//  Created by Jacob Christie on 2020-07-16.
+//  Copyright © 2020 Jacob Christie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
